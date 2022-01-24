dcac92beedaac9b09e7bd6bd40d060d997451f70
Minjune <minjune8506@gmail.com>
2022-01-24 22:59:29 +0900

    array test code

--- array.c
+++ array.c
@@ -4,7 +4,7 @@ int main(void)
 {
 	int arr[5] = {1, 2, 3, 4, 5};
 
-	printf("%p\n", &arr[0]); // 주소가 4씩 증가한다.
+	printf("%p\n", &arr[0]); // 주소가 4씩 증가.
 	printf("%p\n", &arr[1]);
 	printf("%p\n", &arr[2]);
 	printf("%p\n", &arr[3]);
